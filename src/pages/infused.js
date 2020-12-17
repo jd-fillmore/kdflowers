33ef651a2c48ed09eb305268646dcd6b4f0fbe57
JD Fillmore <jd@jdfill.me>
2020-12-17 14:36:39 -0500

    add seo links

--- src/pages/infused.js
+++ src/pages/infused.js
@@ -4,84 +4,6 @@ import InnerHero from "../components/inner-hero/inner-hero"
 import { Container, Row, Col } from "reactstrap"
 import CTA from "../components/cta/cta"
 
-const citiesNiagara = [
-  {
-    name: "Niagara Web Design & SEO",
-    link: "https://infused.agency/",
-  },
-  {
-    name: "Wainfleet Web Design",
-    link: "https://infused.agency/wainfleet-web-design",
-  },
-  {
-    name: "Port Colborne Web Design & SEO",
-    link: "https://infused.agency/port-colborne-web-design",
-  },
-  {
-    name: "Beamsville-Lincoln Web Design & SEO",
-    link: "https://infused.agency/beamsville-lincoln-web-design",
-  },
-  {
-    name: "Grimsby Web Design & SEO",
-    link: "https://infused.agency/grimsby-web-design",
-  },
-  {
-    name: "Fort Erie Web Design & SEO",
-    link: "https://infused.agency/fort-erie-web-design",
-  },
-  {
-    name: "Thorold Web Design & SEO",
-    link: "https://infused.agency/thorold-web-design",
-  },
-  {
-    name: "Smithville Web Design & SEO",
-    link: "https://infused.agency/smithville-web-design",
-  },
-  {
-    name: "Pelham Web Design & SEO",
-    link: "https://infused.agency/pelham-web-design",
-  },
-  {
-    name: "Niagara-on-the-Lake Web Design & SEO",
-    link: "https://infused.agency/niagara-on-the-lake-web-design",
-  },
-]
-
-const citiesHamilton = [
-  {
-    name: "Hamilton Web Design & SEO",
-    link: "https://infused.agency/hamilton-web-design",
-  },
-  {
-    name: "Waterdown Web Design & SEO",
-    link: "https://infused.agency/waterdown-web-design",
-  },
-  {
-    name: "Dundas Web Design & SEO",
-    link: "https://infused.agency/dundas-web-design",
-  },
-  {
-    name: "Ancaster Web Design & SEO",
-    link: "https://infused.agency/ancaster-web-design",
-  },
-  {
-    name: "Stoney Creek Web Design & SEO",
-    link: "https://infused.agency/stoney-creek-web-design",
-  },
-  {
-    name: "Burlington Web Design & SEO",
-    link: "https://infused.agency/burlington-web-design",
-  },
-  {
-    name: "Caledonia Web Design & SEO",
-    link: "https://infused.agency/caledonia-web-design",
-  },
-  {
-    name: "Binbrook Web Design & SEO",
-    link: "https://infused.agency/binbrook-web-design",
-  },
-]
-
 const Infused = () => (
   <>
     <Nav />
@@ -90,34 +12,26 @@ const Infused = () => (
       <Container>
         <Row>
           <Col lg="12">
-            <p>A Niagara-based web design, development &amp; SEO company.</p>
-            <p>Areas served:</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg="6">
-            <h5>Niagara</h5>
-            <ul>
-              {citiesNiagara.map(city => (
-                <li>
-                  <a href={city.link} rel="noreferrer" target="_blank">
-                    {city.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </Col>
-          <Col lg="6">
-            <h5>Hamilton</h5>
-            <ul>
-              {citiesHamilton.map(city => (
-                <li>
-                  <a href={city.link} rel="noreferrer" target="_blank">
-                    {city.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
+            <p>
+              KD Flowers hired Infused Agency to help build them a modern
+              website. Along with this, they wanted to rank in Google for a few
+              different local keywords. The idea was to get a reputable website
+              up and running, but to also attract local clients.
+            </p>
+            <p>
+              At the time of this post, KD Flowers ranks on the first page of
+              Google for a myriad of local keywords that her customers search
+              for. She continues to get many leads a month solely through SEO
+              efforts.
+            </p>
+            <p>
+              If you're a business looking for a new website, or you'd like to
+              rank high in Google for more customers to find you which will lead
+              to more revenue, contact one of the top{" "}
+              <a href="https://infused.agency">Niagara web design</a> and{" "}
+              <a href="https://infused.agency">Niagara SEO</a> companies for a
+              free quote today.
+            </p>
           </Col>
         </Row>
       </Container>
