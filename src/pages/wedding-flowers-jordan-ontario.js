cecb523f7139bb800956e28a65d9d34d210e844e
JD Fillmore <jd@jdfill.me>
2020-12-22 23:18:31 -0500

    seo pages

---
+++ src/pages/wedding-flowers-jordan-ontario.js
@@ -0,0 +1,65 @@
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import "../components/button.scss"
+import "../components/breakpoints.scss"
+import "../components/general.scss"
+import "../components/typography.scss"
+import "../components/variables.scss"
+
+import Hero from "../components/hero/hero"
+import Nav from "../components/nav/nav"
+import Intro from "../components/intro/intro"
+import About from "../components/about/about"
+import Process from "../components/process/process"
+import Testimonial from "../components/testimonial/testimonial"
+import CTA from "../components/cta/cta"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Wedding Flowers &amp; Florist in Jordan, Ontario | Affordable Bridal Bouquets" />
+    <Nav />
+    <Hero
+      title="Wedding Flowers &amp; Florist in Jordan, Ontario | Affordable Bridal Bouquets"
+      subTitle="Quality wedding flowers &amp; bridal bouquets serving Jordan, Ontario at an affordable price"
+      btnText="Book A Consult"
+    />
+    <Intro
+      title="Jordan, Ontario Wedding Flowers &amp; Bridal Bouquets"
+      text="KD Flowers wedding floral design focuses on one goal - creating lasting beautiful memories by designing personalized, elegant and lavish wedding flowers for brides and grooms all over Jordan, Ontario. Designing and creating wedding flowers is my passion and I do my best to make sure your wedding flowers reflect your style and love for each other. I work to capture all your wedding fantasies and make them a dream come true!"
+      btnText="View Portfolio"
+      imgAlt="wedding flowers Jordan, Ontario, Jordan, Ontario florist"
+    />
+    <About
+      title="Kylee Danielle"
+      subTitle="Owner of KD Flowers"
+      imgAlt="affordable cheap bridal bouquets"
+      btnText="Book A Consult"
+    />
+    <Process
+      mainTitle="My Process"
+      mainSubTitle="How I can help you"
+      titleOne="Consultation"
+      titleTwo="Plan"
+      titleThree="Ordering Process"
+      titleFour="Design &amp; Create"
+      textOne="Generally I prefer to meet my Jordan, Ontario bride in person to discuss their
+      vision for their big day! If that is not a possibility, I have no
+      problem connecting via e-mail, phone call or text."
+      textTwo="Once I have an idea of your vision, I do my best to put it into perspective. Collecting all of your thoughts and imaginative ideas helps me give suggestions and envision what you are dreaming for your special day! Once I know what flowers, design, and style you fantasize about, I process an estimate which includes cost of flowers, labour and delivery for your Jordan, Ontario wedding."
+      textThree="I work out of my home, therefore I do not have my own warehouse full of flowers (as much as I would love that!). I always tell my Jordan, Ontario brides they are welcome to have their flowers purchased from wherever they prefer. If there is no preference, I order from a local Jordan, Ontario florist who has been very helpful and accommodating."
+      textFour="Once I have everything in order, I put my heart and soul into designing and creating your Jordan, Ontario wedding flowers!"
+    />
+    <Testimonial
+      title="Client Testimonial"
+      text="Kylee did an amazing job on your flowers! They turned out stunning! She was always answering my silly questions I might have and is amazing to work with. Amazing prices for what we requested. Thank you Kylee for our beautiful flowers!!"
+      author="Denice V."
+      imgAlt="KD Flowers Testimonial"
+    />
+    <CTA />
+  </Layout>
+)
+
+export default IndexPage
---
+++ src/pages/wedding-flowers-port-dalhousie.js
@@ -0,0 +1,65 @@
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import "../components/button.scss"
+import "../components/breakpoints.scss"
+import "../components/general.scss"
+import "../components/typography.scss"
+import "../components/variables.scss"
+
+import Hero from "../components/hero/hero"
+import Nav from "../components/nav/nav"
+import Intro from "../components/intro/intro"
+import About from "../components/about/about"
+import Process from "../components/process/process"
+import Testimonial from "../components/testimonial/testimonial"
+import CTA from "../components/cta/cta"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Wedding Flowers &amp; Florist in Port Dalhousie | Affordable Bridal Bouquets" />
+    <Nav />
+    <Hero
+      title="Wedding Flowers &amp; Florist in Port Dalhousie | Affordable Bridal Bouquets"
+      subTitle="Quality wedding flowers &amp; bridal bouquets serving Port Dalhousie at an affordable price"
+      btnText="Book A Consult"
+    />
+    <Intro
+      title="Port Dalhousie Wedding Flowers &amp; Bridal Bouquets"
+      text="KD Flowers wedding floral design focuses on one goal - creating lasting beautiful memories by designing personalized, elegant and lavish wedding flowers for brides and grooms all over Port Dalhousie. Designing and creating wedding flowers is my passion and I do my best to make sure your wedding flowers reflect your style and love for each other. I work to capture all your wedding fantasies and make them a dream come true!"
+      btnText="View Portfolio"
+      imgAlt="wedding flowers Port Dalhousie, Port Dalhousie florist"
+    />
+    <About
+      title="Kylee Danielle"
+      subTitle="Owner of KD Flowers"
+      imgAlt="affordable cheap bridal bouquets"
+      btnText="Book A Consult"
+    />
+    <Process
+      mainTitle="My Process"
+      mainSubTitle="How I can help you"
+      titleOne="Consultation"
+      titleTwo="Plan"
+      titleThree="Ordering Process"
+      titleFour="Design &amp; Create"
+      textOne="Generally I prefer to meet my Port Dalhousie bride in person to discuss their
+      vision for their big day! If that is not a possibility, I have no
+      problem connecting via e-mail, phone call or text."
+      textTwo="Once I have an idea of your vision, I do my best to put it into perspective. Collecting all of your thoughts and imaginative ideas helps me give suggestions and envision what you are dreaming for your special day! Once I know what flowers, design, and style you fantasize about, I process an estimate which includes cost of flowers, labour and delivery for your Port Dalhousie wedding."
+      textThree="I work out of my home, therefore I do not have my own warehouse full of flowers (as much as I would love that!). I always tell my Port Dalhousie brides they are welcome to have their flowers purchased from wherever they prefer. If there is no preference, I order from a local Port Dalhousie florist who has been very helpful and accommodating."
+      textFour="Once I have everything in order, I put my heart and soul into designing and creating your Port Dalhousie wedding flowers!"
+    />
+    <Testimonial
+      title="Client Testimonial"
+      text="Kylee did an amazing job on your flowers! They turned out stunning! She was always answering my silly questions I might have and is amazing to work with. Amazing prices for what we requested. Thank you Kylee for our beautiful flowers!!"
+      author="Denice V."
+      imgAlt="KD Flowers Testimonial"
+    />
+    <CTA />
+  </Layout>
+)
+
+export default IndexPage
